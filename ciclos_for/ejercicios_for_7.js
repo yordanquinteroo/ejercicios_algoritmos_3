1992f0ab0d0c9a4ce98ac6e1c9ec007db4faa487
= <=>
2026-07-31 16:17:14 -0500

    CICLOS.FOR 7

--- ciclos_for/ejercicios_for_7.js
+++ ciclos_for/ejercicios_for_7.js
@@ -1,1 +1,9 @@
 //Interrupción con break: Crea un ciclo del 1 al 10 que se detenga inmediatamente y salga del bucle cuando el contador sea igual a 7.
+
+for (let i = 1; i <= 10; i++) {
+    if (i === 7) {
+        break;
+    }
+
+    console.log(i);
+}
