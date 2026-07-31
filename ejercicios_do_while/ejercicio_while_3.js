acb100b023036e232cc27755dcc2588feda0324d
= <=>
2026-07-31 16:36:12 -0500

    while#3

--- ejercicios_do_while/ejercicio_while_3.js
+++ ejercicios_do_while/ejercicio_while_3.js
@@ -1,3 +1,13 @@
 /**
  * Acumulador de suma: Escribe un ciclo que sume los números del 1 al 10 y muestre el total acumulado al finalizar
  */
+
+let contador = 1;
+let suma = 0;
+
+while (contador <= 10) {
+    suma += contador;
+    contador++;
+}
+
+console.log("La suma total es: " + suma);
