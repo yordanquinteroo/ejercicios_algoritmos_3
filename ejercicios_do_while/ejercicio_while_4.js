e202d2ebc98151f976d5abb1cd10b26d2d6e2c96
= <=>
2026-07-31 16:36:49 -0500

    while#4

--- ejercicios_do_while/ejercicio_while_4.js
+++ ejercicios_do_while/ejercicio_while_4.js
@@ -1,3 +1,9 @@
 /**
  * Cuenta regresiva: Realiza un conteo hacia atrás desde el número 15 hasta el 0 utilizando el decremento de la variable de control
  */
+let contador = 15;
+
+while (contador >= 0) {
+    console.log(contador);
+    contador--;
+}
