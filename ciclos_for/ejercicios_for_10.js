566599f06df6ebe92814a53f6bc089b5857102b3
= <=>
2026-07-31 16:23:20 -0500

    CICLOS_FOR 10

--- ciclos_for/ejercicios_for_10.js
+++ ciclos_for/ejercicios_for_10.js
@@ -1,1 +1,11 @@
 //Búsqueda de índice: Dado un arreglo de elementos, utiliza un ciclo para encontrar en qué posición (índice) se encuentra un valor específico y detén la búsqueda una vez hallado.
+
+let frutas = ["Manzana", "Pera", "Uva", "Banano", "Mango"];
+let valorBuscado = "Banano";
+
+for (let i = 0; i < frutas.length; i++) {
+    if (frutas[i] === valorBuscado) {
+        console.log("El elemento se encuentra en la posición: " + i);
+        break;
+    }
+}
