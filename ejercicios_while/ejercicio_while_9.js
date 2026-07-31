c93b2ee0b67b76c69b19c79c43edc344f98ee519
= <=>
2026-07-31 16:46:27 -0500

    WHILE#9

--- ejercicios_while/ejercicio_while_9.js
+++ ejercicios_while/ejercicio_while_9.js
@@ -1,3 +1,10 @@
 /**
  * Detección de Bucle Infinito: Identifica y corrige un código donde la condición x <= 5 sea siempre verdadera porque falta el incremento de la variable, evitando así que el programa se bloquee
  */
+
+let x = 1;
+
+while (x <= 5) {
+    console.log(x);
+    x++;
+}
