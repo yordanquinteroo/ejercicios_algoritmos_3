9710177120ac759d7e60af6ecbe0cd97c4673ea4
= <=>
2026-07-31 16:43:02 -0500

    WHILE#3

--- ejercicios_while/ejercicio_while_3.js
+++ ejercicios_while/ejercicio_while_3.js
@@ -1,3 +1,13 @@
 /*
 Suma Acumulada: Escribe un ciclo que sume los números del 1 al 5 (1+2+3+4+5) y muestre el resultado total al final de la ejecución
 */
+
+let contador = 1;
+let suma = 0;
+
+while (contador <= 5) {
+    suma = suma + contador;
+    contador++;
+}
+
+console.log("La suma total es: " + suma);
