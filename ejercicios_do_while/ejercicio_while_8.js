8c4d0bda582dc9670873ae8a04d624c7945768b2
= <=>
2026-07-31 16:39:10 -0500

    while#8

--- ejercicios_do_while/ejercicio_while_8.js
+++ ejercicios_do_while/ejercicio_while_8.js
@@ -1,3 +1,14 @@
 /**
  * Salida con break: Crea un bucle que cuente del 1 al 50, pero que se detenga y salga completamente del ciclo cuando el contador llegue a 25 usando break
  */
+
+let contador = 1;
+
+while (contador <= 50) {
+    if (contador === 25) {
+        break;
+    }
+
+    console.log(contador);
+    contador++;
+}
