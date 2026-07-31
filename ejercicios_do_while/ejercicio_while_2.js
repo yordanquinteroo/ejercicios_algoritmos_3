c7bb5dbe5313f4209e7b48af0bc055034f71565a
= <=>
2026-07-31 16:35:15 -0500

    while#2

--- ejercicios_do_while/ejercicio_while_2.js
+++ ejercicios_do_while/ejercicio_while_2.js
@@ -1,3 +1,9 @@
 /**
  * Conteo ascendente: Utiliza un ciclo para imprimir los números del 1 al 100 en la consola, asegurándote de incrementar la variable en cada vuelta
  */
+
+let contador = 1;
+
+while (contador <= 100) {
+    console.log(contador);
+    contador++;}
