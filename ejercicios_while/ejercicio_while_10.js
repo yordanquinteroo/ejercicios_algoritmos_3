cea9ffc6e5e4f6056381580550452112baa082e8
= <=>
2026-07-31 16:47:09 -0500

    WHILE#10

--- ejercicios_while/ejercicio_while_10.js
+++ ejercicios_while/ejercicio_while_10.js
@@ -1,3 +1,12 @@
 /**
  * Comparación While vs Do...While: Escribe un ciclo while con una condición que sea falsa desde el inicio (por ejemplo, contador < 0) para observar que no se ejecuta ninguna vez, a diferencia de un do...while
  */
+
+let contador = 0;
+
+while (contador < 0) {
+    console.log(contador);
+    contador++;
+}
+
+console.log("El ciclo ha terminado.");
