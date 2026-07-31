6ccbfe5c5abe44fdec017e6260fa2acc45f9decc
= <=>
2026-07-31 16:41:03 -0500

    DO WHILE #10

--- ejercicios_do_while/ejercicio_while_10.js
+++ ejercicios_do_while/ejercicio_while_10.js
@@ -1,3 +1,14 @@
 /**
  * Detección de paridad: Crea un ciclo que recorra los números del 1 al 20 e imprima solo aquellos que son pares, utilizando una estructura condicional dentro del bloque do
  */
+
+
+let contador = 1;
+
+do {
+    if (contador % 2 === 0) {
+        console.log(contador);
+    }
+
+    contador++;
+} while (contador <= 20);
