7b56f0e9c23db6892d22208c9fe53e9a54e38ba5
= <=>
2026-07-31 16:42:30 -0500

    WHILE#2

--- ejercicios_while/ejercicio_while_2.js
+++ ejercicios_while/ejercicio_while_2.js
@@ -1,3 +1,10 @@
 /*
 El Contenedor de Manzanas: Simula el llenado de un contenedor. Debes imprimir "Manzana número X" desde la 1 hasta llegar a la 1000
 */
+
+let contador = 1;
+
+while (contador <= 1000) {
+    console.log("Manzana número " + contador);
+    contador++;
+}
