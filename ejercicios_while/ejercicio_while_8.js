b10e854672991b6bf82a87d4ec365c4cf8d20f3e
= <=>
2026-07-31 16:45:14 -0500

    WHILE#8

--- ejercicios_while/ejercicio_while_8.js
+++ ejercicios_while/ejercicio_while_8.js
@@ -1,3 +1,15 @@
 /*
 Uso de Continue: Crea un programa que cuente del 1 al 5, pero que "salte" la impresión del número 3 y continúe con los demás números de la secuencia
 */
+
+let contador = 0;
+
+while (contador < 5) {
+    contador++;
+
+    if (contador === 3) {
+        continue;
+    }
+
+    console.log(contador);
+}
