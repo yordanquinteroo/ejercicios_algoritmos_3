2a876bf7919cd4e28bfdf95dfda48f51dc44e63d
= <=>
2026-07-31 16:43:41 -0500

    WHILE#5

--- ejercicios_while/ejercicio_while_5.js
+++ ejercicios_while/ejercicio_while_5.js
@@ -1,3 +1,10 @@
 /**
  * Lista con Espacios: Genera una secuencia de números del 1 al 10 que aparezcan en una sola línea, concatenando cada número con un espacio en blanco
  */
+
+let contador = 10;
+
+while (contador >= 1) {
+    console.log(contador);
+    contador--;
+}
