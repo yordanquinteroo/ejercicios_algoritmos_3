393d5bc3dc1edf3d3795e4e2551e5c1bc9485d68
= <=>
2026-07-31 15:12:51 -0500

    CICLOS FOR1

--- ciclos_for/ejercicios_for_1.js
+++ ciclos_for/ejercicios_for_1.js
@@ -1,6 +1,5 @@
 //Conteo estándar: Crea un bucle que imprima en la consola los números del 1 al 100 de forma consecutiva
 
-let i = 1;
-for (let i = 1; 1 <= 100; 1++) {
-    console.log(1);
+for (let i = 1; i <= 100; i++) {
+    console.log(i);
 }
--- ciclos_for/ejercicios_for_2.js
+++ ciclos_for/ejercicios_for_2.js
@@ -1,1 +1,5 @@
 //Llenado de contenedor: Simula el llenado de un contenedor de manzanas imprimiendo el número de manzana actual desde la 1 hasta la 1000.
+
+for (let i = 1; <= 1000; i++){
+    console.log("Manzana numero: " + i);
+}
