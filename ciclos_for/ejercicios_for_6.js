1696fea337539cc7330c99cf3746314647ff5434
= <=>
2026-07-31 16:14:38 -0500

    CICLOS.FOR6

--- ciclos_for/ejercicios_for_6.js
+++ ciclos_for/ejercicios_for_6.js
@@ -1,1 +1,7 @@
 //Recorrido de arreglos: Define un arreglo de nombres y utiliza un bucle para mostrar cada nombre individualmente en la pantalla.
+
+let nombres = ["Lindsay", "Adriana", "Jhoan", "Carmen", "Anderson"];
+
+for (let i = 0; i < nombres.length; i++) {
+    console.log(nombres[i]);
+}
