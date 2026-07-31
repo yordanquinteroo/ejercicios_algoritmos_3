66e710881d6c1b97700f30bbdc80e42b60fac39c
= <=>
2026-07-31 15:24:23 -0500

    CICLOS FOR 2

--- ciclos_for/ejercicios_for_2.js
+++ ciclos_for/ejercicios_for_2.js
@@ -1,5 +1,5 @@
 //Llenado de contenedor: Simula el llenado de un contenedor de manzanas imprimiendo el número de manzana actual desde la 1 hasta la 1000.
 
-for (let i = 1; <= 1000; i++){
-    console.log("Manzana numero: " + i);
+for (let i = 1; i <= 1000; i++) {
+    console.log("manzana numero: " + i);
 }
