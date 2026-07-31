0a6509837bc43ed02c18681834f1a53169f32bb8
= <=>
2026-07-31 16:39:52 -0500

    while#9

--- ejercicios_do_while/ejercicio_while_9.js
+++ ejercicios_do_while/ejercicio_while_9.js
@@ -1,3 +1,18 @@
 /**
  * Formato de lista: Genera una secuencia de números del 1 al 20 que se impriman en una sola línea, concatenados con un guion ("-") entre ellos
  */
+
+let contador = 1;
+let resultado = "";
+
+while (contador <= 20) {
+    resultado += contador;
+
+    if (contador < 20) {
+        resultado += "-";
+    }
+
+    contador++;
+}
+
+console.log(resultado);
