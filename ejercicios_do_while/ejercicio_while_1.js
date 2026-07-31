5978455fc863a1289d07045f8c880cc996af6dd5
= <=>
2026-07-31 16:34:18 -0500

    while#!

--- ejercicios_do_while/ejercicio_while_1.js
+++ ejercicios_do_while/ejercicio_while_1.js
@@ -1,3 +1,10 @@
 /**
  * Garantía de ejecución: Crea un programa donde la variable de control sea 10 y la condición sea contador < 5. Observa cómo el bloque se ejecuta una vez a pesar de que la condición es falsa desde el inicio
  */
+
+let contador = 10;
+
+do {
+    console.log("El contador es: " + contador);
+    contador++;
+} while (contador < 5);
