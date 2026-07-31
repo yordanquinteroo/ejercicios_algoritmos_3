cd9b254fa567e6358840c6667b8de7c19182e37f
= <=>
2026-07-31 16:18:51 -0500

    CICLOS_FOR 8

--- ciclos_for/ejercicios_for_8.js
+++ ciclos_for/ejercicios_for_8.js
@@ -1,1 +1,9 @@
 //Salto con continue: Escribe un bucle que cuente del 1 al 10, pero que se salte la impresión del número 5 y continúe con los demás.
+
+for (let i = 1; i <= 10; i++) {
+    if (i === 5) {
+        continue;
+    }
+
+    console.log(i);
+}
