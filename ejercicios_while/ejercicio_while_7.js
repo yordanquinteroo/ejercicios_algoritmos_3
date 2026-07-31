a2eaf7a5f3e4208ddbf6614888d162993a773168
= <=>
2026-07-31 16:44:44 -0500

    WHILE#7

--- ejercicios_while/ejercicio_while_7.js
+++ ejercicios_while/ejercicio_while_7.js
@@ -1,3 +1,14 @@
 /**
  * Uso de Break: Escribe un ciclo que cuente del 1 al 10, pero que se interrumpa y salga del bucle inmediatamente cuando el contador llegue a 7
  */
+
+let contador = 1;
+
+while (contador <= 10) {
+    if (contador === 7) {
+        break;
+    }
+
+    console.log(contador);
+    contador++;
+}
