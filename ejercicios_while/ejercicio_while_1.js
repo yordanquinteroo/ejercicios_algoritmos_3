90cd87e4fc888b2153999e5069f30c932c763d2b
= <=>
2026-07-31 16:41:53 -0500

    WHILE#1

--- ejercicios_while/ejercicio_while_1.js
+++ ejercicios_while/ejercicio_while_1.js
@@ -1,3 +1,10 @@
 /*
 Conteo Progresivo: Crea un programa que imprima en la consola los números del 1 al 100 utilizando una variable contador
 */
+
+let contador = 1;
+
+while (contador <= 100) {
+    console.log(contador);
+    contador++;
+}
