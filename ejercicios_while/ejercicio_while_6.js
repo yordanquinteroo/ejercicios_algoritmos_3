5ca86b4272602c3d11375bb6a8d9673c74d5a0ad
= <=>
2026-07-31 16:44:13 -0500

    WHILE#6

--- ejercicios_while/ejercicio_while_6.js
+++ ejercicios_while/ejercicio_while_6.js
@@ -1,3 +1,10 @@
 /**
  * Múltiplos de 5: Crea un bucle que muestre únicamente los múltiplos de 5 que se encuentran entre el 0 y el 50
  */
+
+let contador = 0;
+
+while (contador <= 50) {
+    console.log(contador);
+    contador += 5;
+}
