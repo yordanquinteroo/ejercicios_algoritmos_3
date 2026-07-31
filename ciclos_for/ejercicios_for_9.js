d2a94e920fd265f43a132c1b386c5b4d354de2d5
= <=>
2026-07-31 16:20:51 -0500

    CICLOS_FOR 9

--- ciclos_for/ejercicios_for_9.js
+++ ciclos_for/ejercicios_for_9.js
@@ -1,1 +1,5 @@
 //Tabla de multiplicar: Genera la tabla de multiplicar del número 5 (del 1 al 10) utilizando la actualización del contador en cada iteración.
+
+for (let i = 1; i <= 10; i++) {
+    console.log("5 x " + i + " = " + (5 * i));
+}
