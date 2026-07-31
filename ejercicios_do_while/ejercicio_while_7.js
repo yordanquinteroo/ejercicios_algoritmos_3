27ec841fa5bd1e718611133eaacdd328037aad6b
= <=>
2026-07-31 16:38:39 -0500

    while#7

--- ejercicios_do_while/ejercicio_while_7.js
+++ ejercicios_do_while/ejercicio_while_7.js
@@ -1,3 +1,15 @@
 /**
  * Salto con continue: Escribe un ciclo del 1 al 10 que imprima los números, pero que "salte" la impresión del número 5 usando la sentencia continue
  */
+
+let contador = 0;
+
+while (contador < 10) {
+    contador++;
+
+    if (contador === 5) {
+        continue;
+    }
+
+    console.log(contador);
+}
