e917cca334f2a0b3802ba3df5aae113785b32395
= <=>
2026-07-31 16:37:22 -0500

    while#5

--- ejercicios_do_while/ejercicio_while_5.js
+++ ejercicios_do_while/ejercicio_while_5.js
@@ -1,3 +1,10 @@
 /**
  * Múltiplos específicos: Crea un programa que muestre todos los múltiplos de 4 que se encuentren entre el 0 y el 40
  */
+
+let contador = 0;
+
+while (contador <= 40) {
+    console.log(contador);
+    contador += 4;
+}
