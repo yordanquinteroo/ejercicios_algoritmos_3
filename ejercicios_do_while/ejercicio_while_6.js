5438be3b8321db0c2714e78786cccf9d5afdbf69
= <=>
2026-07-31 16:37:59 -0500

    while#6

--- ejercicios_do_while/ejercicio_while_6.js
+++ ejercicios_do_while/ejercicio_while_6.js
@@ -1,3 +1,10 @@
 /**
  * Simulación de llenado: Siguiendo el ejemplo de las fuentes, simula el llenado de un contenedor imprimiendo "Manzana añadida: X" hasta llegar a 100 manzanas
  */
+
+let contador = 1;
+
+while (contador <= 100) {
+    console.log("Manzana añadida: " + contador);
+    contador++;
+}
